fc8bb3afb81b3e7203f7bcc8ded07e9af225e645
Jackdunstan <jack.dunstan@rmit.edu.au>
2017-11-21 17:17:31 +1100

    Debug ACf

--- functions.php
+++ functions.php
@@ -74,4 +74,37 @@ require get_template_directory() . '/inc/editor.php';
 /**
  *  Load Advanced Custom Fields
  */
-require get_template_directory() . '/acf/acf.php';
+ // 1. customize ACF path
+ add_filter('acf/settings/path', 'my_acf_settings_path');
+
+ function my_acf_settings_path( $path ) {
+
+     // update path
+     $path = get_stylesheet_directory() . '/acf/';
+
+     // return
+     return $path;
+
+ }
+
+
+ // 2. customize ACF dir
+ add_filter('acf/settings/dir', 'my_acf_settings_dir');
+
+ function my_acf_settings_dir( $dir ) {
+
+     // update path
+     $dir = get_stylesheet_directory_uri() . '/acf/';
+
+     // return
+     return $dir;
+
+ }
+
+
+ // 3. Hide ACF field group menu item
+ add_filter('acf/settings/show_admin', '__return_false');
+
+
+ // 4. Include ACF
+ include_once( get_stylesheet_directory() . '/acf/acf.php' );
