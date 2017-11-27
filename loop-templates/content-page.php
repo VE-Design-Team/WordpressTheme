f949e4c40bd2cc4d009562c7a3f9b86cad8b3366
Jackdunstan <jack.dunstan@rmit.edu.au>
2017-11-27 15:52:49 +1100

    ACF now does dynamic cards

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -8,14 +8,6 @@
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
-
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
 	<div class="entry-content">
 
 		<?php the_content(); ?>
@@ -32,10 +24,6 @@
 
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
 
-		<?php edit_post_link( __( 'Edit', 'understrap' ), '<span class="edit-link">', '</span>' ); ?>
-
-	</footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
--- page.php
+++ page.php
@@ -19,7 +19,7 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 
 <div class="wrapper" id="page-wrapper">
 
-	<div class="container" id="content" tabindex="-1">
+	<div class="container-fluid" id="content" tabindex="-1">
 
 		<div class="row">
 
