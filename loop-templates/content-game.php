9fd59d3e12fb0de87274e583173c6ee602526887
Jackdunstan <jack.dunstan@rmit.edu.au>
2017-12-04 12:58:10 +1100

    Improvements to game layout

--- loop-templates/content-game.php
+++ loop-templates/content-game.php
@@ -13,24 +13,24 @@
 		?>
 <div class="parent">
 	<div class="collapse parent " id="collapse<?php echo $row_count; ?>">
+		<?php get_template_part('loop-templates/game/card');?>
 <div class="card">
-  <img class="card-img-top" src="<?php echo $image['url']; ?>" alt="Card image cap">
-    <div class="card-body">
-    <p class="card-text"><h3><?php echo $title; ?></h3><?php echo $content; ?></p>
-<!-- Card <?php echo $row_count; ?>-->
 
-		
+<?php if( get_sub_field('field_5a20996ca0dd0') ): ?>
+	  <img class="card-img-top" src="<?php echo $image['url']; ?>" alt="<?php echo $title; ?>">
+<?php endif; ?>
+
+    <div class="card-body">
+    <h3><?php echo $title; ?></h3><p class="card-text"><?php echo $content; ?></p>
+<!-- Card <?php echo $row_count; ?>-->
+	
 <!-- get button data -->
 <?php if( have_rows('field_5a2099c5a0dd1') ): ?>
-
 	<?php while( have_rows('field_5a2099c5a0dd1') ): the_row(); 
-
 		// vars
 		$button_title = get_sub_field('field_5a209a18a0dd2');
 		$button_location = get_sub_field('field_5a20b601dd5a4');
-
 		?>
-
 <?php if( get_field('field_5a20e2a468c91') == 'button' ): ?>
 		<a class="btn btn-primary" href="#<?php echo $button_location ?>"
 data-toggle="collapse" data-parent="#ChoiceGame" data-target="#collapse<?php echo $button_location ?>" aria-expanded="false" aria-controls="collapse<?php echo $button_location ?> " ">
