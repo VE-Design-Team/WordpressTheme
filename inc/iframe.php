2ff0c3cf7d1d935c0021cc7c8c23caf14d6a4bf4
Jackdunstan <jack.dunstan@rmit.edu.au>
2017-12-05 14:00:09 +1100

    Better Iframe messages for accessibility

--- inc/iframe.php
+++ inc/iframe.php
@@ -1,14 +1,19 @@
 <?php
-function general_admin_notice(){
+// this function outputs Iframe embed code information on relvant pages
+
+
+
+function iframe_admin_notice(){
     global $pagenow;
-    if ( $pagenow == 'post.php' ) {
+    if ( $pagenow == 'post.php' || 'page.php' ) {
+    	$iframe_title = get_field( "field_5a2604a30cdd2" );
          echo '
          <div class="notice notice-warning is-dismissible">
                       <h2>Embed this page in canvas by pasting this code</h2>
-&lt;iframe src="'.get_home_url().'/?p='.get_the_ID().'" style="border:0px #ffffff none;" name="Richcontent" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" height="600px" width="800px" onload="resizeIframe(this)" allowfullscreen&gt;&lt;/iframe&gt;
+&lt;iframe src="'.get_home_url().'/?p='.get_the_ID().'" title="'.$iframe_title.'" style="border:0px #ffffff none;" name="Richcontent" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" height="600px" width="800px" onload="resizeIframe(this)" allowfullscreen&gt;&lt;/iframe&gt;
                   </div>
 
          ';
     }
 }
-add_action('admin_notices', 'general_admin_notice');
+add_action('admin_notices', 'iframe_admin_notice');
