ce6f2384ce38be42def5b48e32edef0a694f3634
Jackdunstan <jack.dunstan@rmit.edu.au>
2017-12-05 14:29:28 +1100

    Improved iframe handling

--- inc/iframe.php
+++ inc/iframe.php
@@ -10,7 +10,7 @@ function iframe_admin_notice(){
          echo '
          <div class="notice notice-warning is-dismissible">
                       <h2>Embed this page in canvas by pasting this code</h2>
-&lt;iframe src="'.get_home_url().'/?p='.get_the_ID().'" title="'.$iframe_title.'" style="border:0px #ffffff none;" name="Richcontent" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" height="600px" width="800px" onload="resizeIframe(this)" allowfullscreen&gt;&lt;/iframe&gt;
+&lt;iframe src="'.get_home_url().'/?p='.get_the_ID().'" title="'.$iframe_title.'" style="border:0px #ffffff none;" name="Richcontent" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" width="800px" onload="resizeIframe(this)"  allowfullscreen&gt;&lt;/iframe&gt;
                   </div>
 
          ';
