58eeb44993e54ab62b1be2fadce141c4783c24c1
Jackdunstan <jack.dunstan@rmit.edu.au>
2017-12-06 15:05:07 +1100

    Added Hava a go and improved styles to card header.

--- loop-templates/acf/cards.php
+++ loop-templates/acf/cards.php
@@ -14,16 +14,33 @@
           $card_header_image = get_sub_field_object('field_5a1b9656a6dc9');
           ?>
         
+ <?php if( get_row_index() % 2 == 0 ){ ?>
+
 <div class="col-12 col-sm-6">
+
+<?php else { ?>
+
+<div class="col-8">
+<?php } ?>
   <div class="card">
     <div class="card-header">
 
 <?php 
   if(get_sub_field('field_5a1b730bb4a87') != "custom"): ?>
    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/<?php echo $card_image ?>.png" alt="<?php echo $card_title; ?>" class="float-left" width="50" height="50" />
+   <h3 class="float-left ml-2 mt-2"><?php echo $card_title; ?>
+    
+   <?php else:{ ?>
+     <h3 class="float-left ml-2 mt-2">   <?php echo get_sub_field('field_5a1b95a1a6dc8'); ?></h3>
+
+    <?php };?>
     <?php endif; ?>
+    
+
+
+    
+
       
-      <h3 class="float-left ml-2 mt-2"><?php echo $card_title; ?></h3>
     </div>
     <div class="card-body">
 
