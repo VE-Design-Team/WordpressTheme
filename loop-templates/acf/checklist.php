b27ae8a08c0b3e61741f6a7fa164069f5f2d8ae7
Jack Dudunstan <jack@dunsta.net>
2018-01-03 14:53:10 +1100

    Disabled collapse toggle

--- loop-templates/acf/checklist.php
+++ loop-templates/acf/checklist.php
@@ -7,11 +7,11 @@ $last = count( get_field('field_5a1dfb52cb056') );
 while(has_sub_field('field_5a1dfb52cb056')):
 ?>
   		<?php if ($i == 1){echo "<div>";}else{
-echo "<div class='collapse' id='collapse".$i."'aria-expanded='false'>";
+echo "<div class='collapse' id='collapse".$i."' aria-expanded='false'>";
   		}?>
   		    <li>
               <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" value="" data-toggle="collapse" data-target="#collapse<?php echo $i+1; ?>"  <?php if ($i == $last){echo "checked";}?>> <?php echo get_sub_field("field_5a1e03efcb057")?>
+                <input class="form-check-input" type="checkbox" value="" data-toggle="collapse" data-target="#collapse<?php echo $i+1; ?>"  <?php if ($i == $last){echo "checked";}?><i></i> <?php echo get_sub_field("field_5a1e03efcb057")?>
               </label>
              
             </li>
@@ -23,8 +23,13 @@ else :
     // no rows found
 endif;
 ?>
-  </ul>
+</ul>
 
+<script>
+$('#collapse2,#collapse3,#collapse4,#collapse5,#collapse6,#collapse7,#collapse8,#collapse9,#collapse10,#collapse11,#collapse12').on('hide.bs.collapse', function (e) {
+  preventDefault(e);
+})
+</script>
 
 
      
