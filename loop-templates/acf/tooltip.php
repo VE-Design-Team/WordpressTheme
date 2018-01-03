bc22492d9192ea5dc7e5c1a21d6ec90f02e40740
Jack Dudunstan <jack@dunsta.net>
2018-01-03 15:35:44 +1100

    Improvements to word hover

--- loop-templates/acf/tooltip.php
+++ loop-templates/acf/tooltip.php
@@ -1,19 +1,18 @@
-  <style><?php if( have_rows('field_5a30b394101e4') ): ?>
-<?php while( have_rows('field_5a30b394101e4') ): the_row();?>
+  <style>
+<?php if( have_rows('field_5a30b394101e4') ):
+  $colorhover = 0; 
+  while( have_rows('field_5a30b394101e4') ): the_row(); 
+    if( get_sub_field('field_5a30b4531abcd') ) $colorhover++;
+    echo "#hover".$colorhover.":hover";
+    echo "{color: ".get_sub_field('field_5a30b4531abcd').";}";
+ endwhile; 
+  $tCount = $colorhover;
 
-.<?php echo str_replace(' ', '', get_sub_field('field_5a30b4461abcc')); ?>:hover
- {
- 	color:<?php echo the_sub_field('field_5a30b4531abcd');?>;
- } 	
- 
-
-<?php endwhile; ?>
-  
-<?php endif; ?>
+endif; ?>
 </style>
 
-<?php if( have_rows('field_5a30b394101e4') ): ?>
-  <p class="display-2 text-center"><?php while( have_rows('field_5a30b394101e4') ): the_row();?><?php get_template_part('loop-templates/acf/tooltip-prepend');?><span title="<?php echo the_sub_field('field_5a30b4461abcc');?>" data-toggle="tooltip" data-placement="top" class="<?php echo str_replace(' ', '', get_sub_field('field_5a30b4461abcc')); ?>" ><?php the_sub_field('field_5a30b40b1abc9')?></span><?php get_template_part('loop-templates/acf/tooltip-append');?><?php endwhile; ?></p>
+<?php if( have_rows('field_5a30b394101e4') ): $colorhovercount = 0; ?>
+  <p class="display-2 text-center"><?php while( have_rows('field_5a30b394101e4') ): the_row();?><?php get_template_part('loop-templates/acf/tooltip-prepend');?><?php  if( get_sub_field('field_5a30b4531abcd') ) $colorhovercount++; ?><span title="<?php echo the_sub_field('field_5a30b4461abcc');?>" data-toggle="tooltip" data-placement="top" id="hover<?php echo $colorhovercount; ?>" class="<?php echo str_replace(' ', '', get_sub_field('field_5a30b4461abcc')); ?>" ><?php the_sub_field('field_5a30b40b1abc9')?></span><?php get_template_part('loop-templates/acf/tooltip-append');?><?php endwhile; ?></p>
 <?php endif; ?>
 
 <script>
