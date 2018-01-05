bc5e5376d4c35805d7e5416407d101194d97d9a3
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-01-05 14:46:34 +1100

    Avoid bugs in edge cases
    Tabs with special characters and character limits

--- loop-templates/acf/tabs-left.php
+++ loop-templates/acf/tabs-left.php
@@ -5,7 +5,7 @@
 
   <?php while ( have_rows('field_5a13c7fda9110') ) : the_row();?>
 
-  <a class="h4 nav-link text-right h5 " id="v-pills-home-tab" data-toggle="pill" href="#<?php echo str_replace(str_split('\\/:*?"<>,| '), '', get_sub_field('field_5a14fdfcc7bba')); ?>" role="tab" aria-controls="v-pills-home" aria-selected="true"><?php echo the_sub_field('field_5a14fdfcc7bba');?></a>
+  <a class="h4 nav-link text-right h5 " id="v-pills-home-tab" data-toggle="pill" href="#<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a14fdfcc7bba')); ?>" role="tab" aria-controls="v-pills-home" aria-selected="true"><?php echo the_sub_field('field_5a14fdfcc7bba');?></a>
   
 <?php endwhile;?>
 </div>
@@ -14,7 +14,7 @@
 
   <?php while ( have_rows('field_5a13c7fda9110') ) : the_row();?>
 
-  <div class="tab-pane fade " id="<?php echo str_replace(str_split('\\/:*?"<>,| '), '', get_sub_field('field_5a14fdfcc7bba')); ?>" role="tabpanel" aria-labelledby="v-pills-home-tab"><?php echo the_sub_field('field_5a14fdab64ad0');?></div>
+  <div class="tab-pane fade " id="<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a14fdfcc7bba')); ?>" role="tabpanel" aria-labelledby="v-pills-home-tab"><?php echo the_sub_field('field_5a14fdab64ad0');?></div>
   
 <?php endwhile;?>  
 </div>
