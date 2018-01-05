03dac4e24c42bb5eb4295175dbd77aba4d2bdfa2
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-01-05 14:50:25 +1100

    Minor cosmetic improvements to quadrant reveal

--- loop-templates/acf/quadrant-reveal.php
+++ loop-templates/acf/quadrant-reveal.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <?php while ( have_rows( 'field_5a4d9d0dc74ab') ) : the_row();?>
-            <div class="col-5 col-md-4 col-lg-3  square-<?php echo $i; ?>">
+            <div class="col-6 col-md-5 col-lg-4  square-<?php echo $i; ?>">
                 <h3 class="heading text-dark "><?php echo the_sub_field('field_5a4d9ddac74ac'); ?></h3>
                 <p>
                     <?php echo the_sub_field( 'field_5a4d9de3c74ad'); ?>
