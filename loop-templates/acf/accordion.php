77e1af542e3a5cbb5aa4ebdbc4b5515e44064672
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-01-10 13:36:14 +1100

    Fixed bug where commas in accordion titles would affect

--- loop-templates/acf/accordion.php
+++ loop-templates/acf/accordion.php
@@ -9,11 +9,11 @@ if( have_rows('field_5a13c61e91a66') ):
 
         <div class="accord-item p-2"><?php //apply the accordion header ?>
 
-        <a data-toggle="collapse" data-parent="#accordion<?php echo get_the_ID();?>" href="#collapse<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>" aria-controls="collapseOne">
+        <a data-toggle="collapse" data-parent="#accordion<?php echo get_the_ID();?>" href="#collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a13c67d91a67')); ?>" aria-controls="collapseOne">
            <h3 class="h5"><span class='ac-plus '><i class="fa fa-plus" aria-hidden="true"></i></span>  <?php echo the_sub_field('field_5a13c67d91a67'); ?></h3>
         </a>
         <?php // display the accordion collapsed content ?>
-        <div id="collapse<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>" class="collapse mt-3 pl-3" role="tabpanel" aria-labelledby="heading<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>">
+        <div id="collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a13c67d91a67')); ?>" class="collapse mt-3 pl-3" role="tabpanel" aria-labelledby="heading<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>">
         <?php echo the_sub_field('field_5a13c6ac91a68'); ?>
         </div>
       </div>
