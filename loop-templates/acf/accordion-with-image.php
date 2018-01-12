2176a6e86f4024aea2fcfac53d3087476d9debfc
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-01-12 16:33:58 +1100

    Made carousel text areas WYSIWYG

--- loop-templates/acf/accordion-with-image.php
+++ loop-templates/acf/accordion-with-image.php
@@ -1,4 +1,3 @@
-
 <style>
 #awm
 {
@@ -7,7 +6,7 @@
 }
 .background-image img
 {
-  min-height: 400px;
+  min-height: 300px;
   max-height: 600px;
   position: absolute;
 right: 0;
@@ -17,15 +16,21 @@ right: 0;
    min-height: 400px;
   max-height: 600px;
 }
+.card-body
+{
+  padding: 0.5rem;
+}
+.col
+{
+  padding-left: 5px ;
+  padding-right: 5px;
+}
 <?php
-
 // Get colours
-
 if (have_rows('field_5a541f36eef91')):
   $i = 1;
   while (have_rows('field_5a541f36eef91')):
     the_row(); ?>
-
 .aim .card-<?php
     echo $i; ?> {
   border-color: <?php
@@ -56,80 +61,54 @@ if (have_rows('field_5a541f36eef91')):
 else:
 endif;
 ?>
-
 }
 [data-toggle="collapse"][aria-expanded="true"] > h6 > .ac-plus {
     display: inline-block;
     -ms-transform: rotate(45deg);
-
     /* IE 9 */
     -webkit-transform: rotate(45deg);
-
     /* Safari 3-8 */
     transform: rotate(45deg);
 }
-
 img.collapsing {
     -webkit-transition: none;
     transition: none;
     display: none;
 }
-
 </style>
-
 <div id="awm" class="container aim">
   <div class="row">
     <div class="col-12 text-center">
       <h3><?php
 the_field('field_5a53f3ef29cd8'); ?></h3>
     </div>
-
-
-    <div class="col-8 background-image "><img src="<?php
+    <div class="col-6 col-md-8 background-image "><img src="<?php
 the_field('field_5a5443ce64623'); ?>" alt="<?php
 the_field('field_5a53f3ef29cd8'); ?>" height="400" class="float-right"/>
-
 <?php
-
 // check if the repeater field has rows of data
-
 if (have_rows('field_5a541f36eef91')):
-
   // ouput backgrounds
-
   $i = 1;
   while (have_rows('field_5a541f36eef91')):
     the_row(); ?>
-
 <?php 
-
 $image = get_sub_field('field_5a5423847dc95');
-
-if( !empty($image) ): ?>
-
+if (!empty($image)): ?>
   <img src="<?php echo $image['url']; ?>" class="img-fluid collapse multi-collapse aim-<?php
     echo $i; ?>" height="400"/>
-
 <?php endif; ?>
-        
 <?php
     $i++;
   endwhile;
 else:
 endif;
 ?>
-
     </div>
-
-
-    <div class="col-4 accordion-area">
-
+    <div class="col-6 col-md-4 accordion-area">
 <?php
-
 if (have_rows('field_5a541f36eef91')):
-
   // output cards and card bodies
-
   $i = 1;
   while (have_rows('field_5a541f36eef91')):
     the_row(); ?>
@@ -138,16 +117,13 @@ if (have_rows('field_5a541f36eef91')):
         <div data-toggle="collapse"  data-target=".aim-<?php
     echo $i; ?>" role="button" aria-expanded="false" aria-controls="aim-<?php
     echo $i; ?>" class="card-header text-white h6">
-
      <span class="ac-plus">+  </span> <?php
     the_sub_field('field_5a541fbfeef92'); ?></div>
-        
-<?php if( get_sub_field('field_5a541fc6eef93') ): ?>
+<?php if (get_sub_field('field_5a541fc6eef93')): ?>
  <div class="aim-<?php
     echo $i; ?> card-body collapse multi-collapse small"><?php
     the_sub_field('field_5a541fc6eef93'); ?></div>
 <?php endif; ?>
-
       </div>     
 <?php
     $i++;
@@ -155,12 +131,6 @@ if (have_rows('field_5a541f36eef91')):
 else:
 endif;
 ?>
-
     </div>
   </div>
 </div>
-
- 
-
- 
-
