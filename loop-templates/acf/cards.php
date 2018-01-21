6566ecea9ab62f493a450f1c906364c18c5c865b
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-01-22 09:13:05 +1100

    Better rendering of icons in card headers

--- loop-templates/acf/cards.php
+++ loop-templates/acf/cards.php
@@ -1,7 +1,14 @@
 <!-- cards.php start -->
-<?php if( have_rows('field_5a1b72e2b4a86') ): ?>
+<style>
+.card-header > img
+{
+  image-rendering: crisp-edges;
+  image-rendering: -webkit-optimize-contrast;
+}
+</style>
+<?php if (have_rows('field_5a1b72e2b4a86')): ?>
 <div class="card-deck glossarize">  
-  <?php while( have_rows('field_5a1b72e2b4a86') ): the_row();?>
+  <?php while (have_rows('field_5a1b72e2b4a86')): the_row();?>
     <?php
           // set variables
           $card_image = get_sub_field('field_5a1b730bb4a87');
@@ -11,12 +18,12 @@
           $card_content = get_sub_field('field_5a1b865eaeaab');
           $card_text_title = get_sub_field_object('field_5a1b95a1a6dc8');
           $card_header_image = get_sub_field_object('field_5a1b9656a6dc9');
-          $numrows = count( get_sub_field( 'field_5a1b72e2b4a86' ) );
+          $numrows = count(get_sub_field('field_5a1b72e2b4a86'));
           ?>
   <div class="card">
     <div class="card-header">
 <?php 
-  if(get_sub_field('field_5a1b730bb4a87') != "custom"): ?>
+  if (get_sub_field('field_5a1b730bb4a87') != "custom"): ?>
    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/<?php echo $card_image ?>.png" alt="<?php echo $card_title; ?>" class="float-left" width="35" height="35" />
    <h3 class="float-left ml-2 mt-2 h5"><?php echo $card_title; ?>
    <?php else:{ ?>
