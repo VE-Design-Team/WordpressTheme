c58a53fa1868a96541699da1f4225c26c23ef9fb
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-01-25 12:17:49 +1100

    Workaround for tooltip conflict

--- footer.php
+++ footer.php
@@ -9,36 +9,30 @@
 $the_theme = wp_get_theme();
 ?>
 <?php wp_footer(); ?>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.min.js"></script>
+<?php if (get_field('field_5a13c5a7ec802') == 'Tooltip Textstrings'): ?>
+<?php else: {} ?>
+  <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.min.js"></script>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/tooltip.js"></script>
+<?php endif;?>
   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.glossarize.js"></script>
   <script>
-
   $(function(){
-
     $('.glossarize').glossarizer({
-      
-    	<?php if (WP_DEBUG === true) {
+      <?php if (WP_DEBUG === true) {
     echo "//dev environment
     ";
     echo "sourceURL: '".get_site_url()."/glossarydata/',";
 } else {
     echo "//live environment
-	 ";
+   ";
     echo "sourceURL: '".get_site_url()."/index.php/glossarydata/',";
 } ?>
-
-  
-
       //sourceURL: '<?php echo get_stylesheet_directory_uri(); ?>/glossary.json',
       callback: function(){
         new tooltip();
       }
     });
-
-
   });
-
   </script>
 </body>
 </html>
