610a2a6bf2746d5895f2ca998130655a08f4e68e
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-01-30 14:34:03 +1100

    Accessibility in checklist
    Thanks Michael

--- loop-templates/acf/checklist.php
+++ loop-templates/acf/checklist.php
@@ -6,7 +6,7 @@ $last = count(get_field('field_5a1dfb52cb056'));
 while (has_sub_field('field_5a1dfb52cb056')):
 ?>
      <div>
-          <li data-toggle="collapse" data-target="#collapse<?php echo $i; ?>">
+          <li tabindex="0" data-toggle="collapse" data-target="#collapse<?php echo $i; ?>">
 <i class="fa fa-square-o" aria-hidden="true"></i><i class="fa fa-check-square-o" aria-hidden="true"></i><span aria-hidden="true"> <?php echo get_sub_field("field_5a1e03efcb057")?></span>
             </li>
           </div>
@@ -23,7 +23,7 @@ endif;
 $('#collapse2,#collapse3,#collapse4,#collapse5,#collapse6,#collapse7,#collapse8,#collapse9,#collapse10,#collapse11,#collapse12').on('hide.bs.collapse', function (e) {
   preventDefault(e);
 })
-$("li:nth-last-child(1)").click(function(){
+$("li:nth-last-child(1)").focus(function(){
     $(this).addClass("checked");
 });
 </script>
