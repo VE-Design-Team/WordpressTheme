18f1ebc5cecef27b96d30d791577394b58ca378c
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-02-16 13:15:17 +1100

    google tag manager

--- header.php
+++ header.php
@@ -27,6 +27,13 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 	<!-- ensure iframe links are opened in a new tab in the parent -->
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WDGXMTN');</script>
+<!-- End Google Tag Manager -->
 	<base target="_blank">
 	   <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.min.js"></script>
 	<?php wp_head(); ?>
