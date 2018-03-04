35d65ce15cf5507086e75abda8748fdb4d7c1228
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-03-05 10:12:18 +1100

    New style for accordion

--- loop-templates/acf/accordion.php
+++ loop-templates/acf/accordion.php
@@ -1,51 +1,25 @@
-<style>
-
-
-
-</style>  
-
+<?php if (get_field('field_5a98cf8733d10')) : ?>
 <div id="instruction"><i class="fa fa-info-circle mr-1 fa-lg"></i>
-  Select each communication or cultural barrier to learn how to manage it.
-</div>
+  <?php echo the_field('field_5a98cf8733d10'); ?>
+  </div>
+<?php endif; ?>
+<?php
+
+if (have_rows('field_5a13c61e91a66')): ?>
 <div id="accordion">
   <div id="cards">
+   <?php while (have_rows('field_5a13c61e91a66')): the_row();?>
     <div class="card">
-      <div class="card-header collapsed pl-2" id="headingOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><i class="fa fa-plus mr-1"></i><i class="fa fa-minus mr-1 mr-1"></i> Collapsible Group Item #1
-
+      <div class="card-header collapsed pl-2" id="heading<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>" data-toggle="collapse" data-target="#collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a13c67d91a67')); ?>" aria-expanded="true" aria-controls="collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a13c67d91a67')); ?>"><i class="fa fa-plus mr-1"></i><i class="fa fa-minus mr-1 mr-1"></i> Collapsible Group Item #1
       </div>
-
-      <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-        <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-          on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table,
-          raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      <div id="collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a13c67d91a67')); ?>" class="collapse" aria-labelledby="heading<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>" data-parent="#accordion">
+        <div class="card-body glossarize">
+        <?php echo the_sub_field('field_5a13c6ac91a68'); ?>
         </div>
       </div>
     </div>
-    <div class="card">
-      <div class="card-header collapsed pl-2" id="headingTwo" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"><i class="fa fa-plus mr-1"></i><i class="fa fa-minus mr-1 mr-1"></i> Collapsible Group Item #2
-
-      </div>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-        <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-          on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table,
-          raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-header collapsed pl-2" id="headingThree" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree"><i class="fa fa-plus mr-1"></i><i class="fa fa-minus mr-1 mr-1"></i> Collapsible Group Item #3
-        </button>
-        </h5>
-      </div>
-      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-        <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-          on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table,
-          raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-        </div>
-      </div>
-    </div>
-  </div>
+				   <?php endwhile; ?>
+           </div>
 </div>
+<?php else: ?>
+<?php endif;?>
