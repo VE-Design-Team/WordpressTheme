6aa6aa76a1298ae07aa7128795e561e39ff6f945
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-03-07 13:29:02 +1100

    Intro tabs and glossary styles

--- loop-templates/acf/intro_page.php
+++ loop-templates/acf/intro_page.php
@@ -1,3 +1,6 @@
+
+
+<!-- new-->
 <style>
 .tab-content
 {
@@ -5,46 +8,51 @@
 }
 .icon > img
 {
-	max-width: 100px;
+  max-width: 100px;
 }
-</style><div class="intro_page">
-<ul class="nav nav-tabs col-12" id="Tab<?php echo get_the_ID();?>" role="tablist">
-<?php while (have_rows('field_5a67bc57f0b60')) : the_row();?>
+</style>
 
-<li class="nav-item ">
-    <a class="nav-link" id="<?php echo str_replace(' ', '', get_sub_field('field_5a67bc81f0b61')); ?>-tab" data-toggle="tab" href="#<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a67bc81f0b61')); ?>" role="tab" aria-controls="<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a67bc81f0b61')); ?>" aria-selected="true"><?php echo the_sub_field('field_5a67bc81f0b61');?> 
-    </a>
-  </li>
-<?php endwhile;?>
-</ul>
-<div class="tab-content col-12 mb-1" id="TabContent<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a13c67d91a67')); ?>">
-<?php while (have_rows('field_5a67bc57f0b60')) : the_row();?>
-	<?php //icon mapping
-$title = get_sub_field('field_5a67bc81f0b61');
-if ($title == "Learning objectives") {
-    $icon = "licensing_accreditation.png";
-    $background = "bg-success";
-} elseif ($title == "Key activities") {
-    $icon = "learning_activity.png";
-    $background = "bg-warning";
-} elseif ($title == "Assessments") {
-    $icon = "assignment.png";
-    $background = "bg-info";
-}
+<div id="tabs">
+  <ul class="nav nav-tabs" role="tablist">
+  <?php while (have_rows('field_5a67bc57f0b60')) : the_row();?>
+
+    <li class="nav-item"><a class="nav-link" id="<?php echo str_replace(' ', '', get_sub_field('field_5a67bc81f0b61')); ?>-tab" data-toggle="tab" href="#<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a67bc81f0b61')); ?>" role="tab" aria-controls="<?php echo str_replace(' ', '', get_sub_field('field_5a67bc81f0b61')); ?>" aria-selected="true"> 
+        <div class="title"><?php echo the_sub_field('field_5a67bc81f0b61');?>  <i class="fa fa-chevron-down fa-sm"></i></div></a></li>
+
+        <?php endwhile;?>
+  </ul>
+  <div class="tab-content glossarize" id="myTabContent<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a13c67d91a67')); ?>">
+  <?php while (have_rows('field_5a67bc57f0b60')) : the_row();?>
+
+  	<?php //icon mapping
+//$title = get_sub_field('field_5a67bc81f0b61');
+//if ($title == "Learning objectives") {
+  //  $icon = "licensing_accreditation.png";
+   // $background = "bg-success";
+//} elseif ($title == "Key activities") {
+ //   $icon = "learning_activity.png";
+  //  $background = "bg-warning";
+//} elseif ($title == "Assessments") {
+ //   $icon = "assignment.png";
+ //   $background = "bg-info";
+//}
 ?>
-      <div class="tab-pane fade show  col-12" id="<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a67bc81f0b61')); ?>" role="tabpanel" aria-labelledby="<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a67bc81f0b61')); ?>-tab">
-      	<div class="row align-items-center <?php echo $background;?>">
+    <div class="tab-pane fade show" id="<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a67bc81f0b61')); ?>" role="tabpanel" aria-labelledby="<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a67bc81f0b61')); ?>-tab">
+    <div class="row align-items-center <?php echo $background;?>">
       		   	<div class="col-9 col-sm-10 p-3 bg-white">
       	<h3 class="text-dark"><?php echo the_sub_field('field_5a67bc81f0b61');?> </h3>
       	<?php echo the_sub_field('field_5a67c80324ecd');?>
       </div>
-<div class="col-3 col-sm-2 text-center icon">
+<!--<div class="col-3 col-sm-2 text-center icon">
 	<img src="<?php echo get_stylesheet_directory_uri();?>\img\<?php echo $icon; ?>" alt="<?php echo the_sub_field('field_5a67bc81f0b61');?> " class=" aligncenter img-fluid" >
+</div>-->
+      	</div></div>
+ 
+    <?php endwhile;?>
+  </div>
 </div>
-      	</div>
-      </div> 
-<?php endwhile;?>
-</div></div>
+
+
 <script>
 //Make first tab active- top tabs
 $('.nav-item a.nav-link:first').addClass('active');
--- loop-templates/content-glossary.php
+++ loop-templates/content-glossary.php
@@ -1,10 +1,12 @@
 <!-- content-glossary.php -->
+
 <?php $query = new WP_Query(array( 'pagename' => 'glossarydata' ));
         while ($query->have_posts()) {
             $query->the_post(); ?>
 <div class="container-fluid">
 	<div class="row">
 	<div class="col-12 content">
+		<ul class="list-unstyled">
 <?php if (have_rows('field_5a1b4e5ebab7b')): ?>
   <?php //start of loop?>
 	<?php $rowCount = count(get_field('field_5a1b4e5ebab7b')); ?>
@@ -16,10 +18,11 @@
 						$description = get_sub_field('field_5a1b51d28610a');
 						$otherterms = get_sub_field('field_5a81179b7173a');		
 						?>
+						<li id="<?php echo $title; ?>" >
 		<h3 class="h4 mb-0 text-danger"><?php echo $title; ?></h3>
 
 <?php if( get_sub_field('field_5a81179b7173a') ): ?>
-<h4 class="mb-0 small"><i class="text-muted">Other terms: </i><?php echo $otherterms; ?></h4>
+<h4 class="mb-0 small"><i class="text-muted">Other terms: </i><?php echo $otherterms; ?></h4></li>
 <?php endif; ?>
 
 	
@@ -31,6 +34,7 @@
 	<?php endwhile; ?>
   <?php //endof loop?>
 <?php endif; ?>
+				</ul>
 </div>
 </div>
 
@@ -38,3 +42,4 @@
         }
     wp_reset_postdata(); ?>
 <!-- content-glossary.php end-->
+
