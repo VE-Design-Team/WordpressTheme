dba2ccc48b48c85034299ffee69aa259b9cb7ad9
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-03-13 06:05:21 +1100

    Accordion with image

--- loop-templates/acf/accordion-with-image.php
+++ loop-templates/acf/accordion-with-image.php
@@ -31,13 +31,11 @@ img.collapsing {
 </div>
 <div class="col-12">
   <div class="row">
-    <div class="col-12 text-center">
-      <h3><?php
-the_field('field_5a53f3ef29cd8'); ?></h3>
-    </div>
     <div class="col-xs-12 col-sm-6 order-sm-12 background-image "><img src="<?php
 the_field('field_5a5443ce64623'); ?>" alt="<?php
 the_field('field_5a53f3ef29cd8'); ?>" height="400" />
+<!-- get images -->
+
 <?php
 // check if the repeater field has rows of data
 if (have_rows('field_5a541f36eef91')):
@@ -48,7 +46,7 @@ if (have_rows('field_5a541f36eef91')):
 <?php 
 $image = get_sub_field('field_5a5423847dc95');
 if (!empty($image)): ?>
-  <img src="<?php echo $image['url']; ?>" class="img-fluid collapse multi-collapse aim-<?php
+  <img src="<?php echo $image['url']; ?>" id="collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a541fbfeef92')); ?>"  class="img-fluid collapse multi-collapse aim-<?php
     echo $i; ?>" height="400"/>
 <?php endif; ?>
 <?php
@@ -57,6 +55,7 @@ if (!empty($image)): ?>
 else:
 endif;
 ?>
+<!-- end get images -->
     </div>
 
     <div class="col-xs-12 col-sm-6 col-sm-1 accordion">
@@ -64,20 +63,24 @@ endif;
       <?php if (have_rows('field_5a541f36eef91')): ?>
 <div id="accordion">
   <div id="cards">
+  <!-- get text accordion -->
    <?php while (have_rows('field_5a541f36eef91')): the_row();?>
     <div class="card">
-      <div class="card-header collapsed pl-2" id="heading<?php echo str_replace(' ', '', get_sub_field('field_5a541fbfeef92')); ?>" data-toggle="collapse" data-target="#collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a541fbfeef92')); ?> " aria-expanded="true" aria-controls="collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a541fbfeef92')); ?>"><i class="fa fa-plus mr-1"></i><i class="fa fa-minus mr-1 mr-1"></i> <?php echo get_sub_field('field_5a541fbfeef92'); ?>
+      <div class="card-header collapsed pl-2" id="heading<?php echo str_replace(' ', '', get_sub_field('field_5a541fbfeef92')); ?>" role="button" data-toggle="collapse" data-target="#collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a541fbfeef92')); ?> " aria-expanded="true" aria-controls="collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a541fbfeef92')); ?>"><i class="fa fa-plus mr-1"></i><i class="fa fa-minus mr-1 mr-1"></i> <?php echo get_sub_field('field_5a541fbfeef92'); ?>
       </div>
+      <?php if (get_sub_field('field_5a541fc6eef93')): ?>
       <div id="collapse<?php echo str_replace(str_split('\\/:*?"<>,.| '), '', get_sub_field('field_5a541fbfeef92')); ?>" class="collapse" aria-labelledby="heading<?php echo str_replace(' ', '', get_sub_field('field_5a541fbfeef92')); ?>" data-parent="#accordion">
         <div class="card-body glossarize multi-collapse aim-<?php
     echo $i; ?>">
         <?php
     the_sub_field('field_5a541fc6eef93'); ?>
         </div>
-
+    
       </div>
+      <?php endif; ?>
     </div>
 				   <?php endwhile; ?>
+           <!-- end get text accordion -->
   </div>
 </div>
 <?php else: ?>
