ccec9734fb5663b037ec572a75296c9cf3755bd0
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-03-23 13:03:00 +1100

    Flip cards v2 style

--- loop-templates/acf/flip-cards.php
+++ loop-templates/acf/flip-cards.php
@@ -1,74 +1,18 @@
-<style>
-.card-flip {
-  -webkit-perspective: 1000px;
-          perspective: 1000px;
-}
-.card-flip:hover .flip, .card-flip.hover .flip {
-  -webkit-transform: rotateY(180deg);
-          transform: rotateY(180deg);
-}
-
-.card-flip,
-.front,
-.back {
-  width: 100%;
-  height: 480px;
-}
-
-.flip {
-  -webkit-transition: 0.6s;
-  transition: 0.6s;
-  -webkit-transform-style: preserve-3d;
-          transform-style: preserve-3d;
-  position: relative;
-}
-
-.front,
-.back {
-  -webkit-backface-visibility: hidden;
-          backface-visibility: hidden;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-
-.front {
-  z-index: 2;
-  -webkit-transform: rotateY(0deg);
-          transform: rotateY(0deg);
-}
-
-.back {
-  -webkit-transform: rotateY(180deg);
-          transform: rotateY(180deg);
-}
-.card
-{
-	
-	background-repeat: no-repeat;
-	background-size: 300%;
-
-	
-}
-
-
-
-</style>	
 
 
   
-
+<div id="card-flip">
   <?php
 
 
 if( have_rows('field_5a865a7a9f82f') ): ?>
 
-	<div class="container pt-5">
+	<div  class="container pt-5">
 		<div class="row">
  <?php while ( have_rows('field_5a865a7a9f82f') ) : the_row(); ?>
- <div class="col-sm-6 col-lg-4">
+ <div class="col-sm-6 col-lg-4" tabindex="0" >
         		<!-- Card Flip -->
-				<div class="card-flip">
+				<div class="card-flip" >
 					<div class="flip">
 						<div class="front">
 							<!-- front content -->
@@ -80,13 +24,15 @@ if( have_rows('field_5a865a7a9f82f') ): ?>
 							<?php if( get_sub_field('field_5a865a8c9f830') ): ?>
 							<div class="card-body">
 							   
-								 <p class="card-text"><?php the_sub_field('field_5a865a8c9f830'); ?></p>
+								 <p class="card-text text-center"><?php the_sub_field('field_5a865a8c9f830'); ?></p>
 								 </div>
+								 <span class="btn btn-primary btn-sm ">Flip me<span>
 							 </div>
 <?php endif; ?>
 
 
 							</div>
+							
 						</div>
 						<div class="back">
 							<!-- back content -->
@@ -97,7 +43,7 @@ if( have_rows('field_5a865a7a9f82f') ): ?>
 								<?php if( get_sub_field('field_5a86650b13630') ): ?>
 							<div class="card-body">
 							   
-								 <p class="card-text"><?php the_sub_field('field_5a86650b13630'); ?></p>
+								 <p class="card-text text-center"><?php the_sub_field('field_5a86650b13630'); ?></p>
 							
 							 </div>
 <?php endif; ?>
@@ -124,6 +70,7 @@ if( have_rows('field_5a865a7a9f82f') ): ?>
 endif;
 
 ?>
+</div>
 
 
 		<script >
