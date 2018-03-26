5d65808aa15a95334bf8675bcb985611fed43761
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-03-26 17:06:20 +1100

    Minor style improvements
    Bugfix and version bump

--- loop-templates/acf/horizontal-cards.php
+++ loop-templates/acf/horizontal-cards.php
@@ -13,10 +13,10 @@ if( have_rows('field_5a86433eed2a0') ): ?>
   <div class="card col-12 mb-2  ml-0">
       
           <div class="row align-items-center">
-          <div class="col-12 col-sm-4 pr-0">
+          <div class="col-3 col-sm-2 pr-0">
     <img src="<?php the_sub_field('field_5a86434fed2a1'); ?>" alt="<?php echo the_sub_field('field_5a67bc81f0b61');?> " class=" aligncenter img-fluid" >
   </div>
-                 <div class="col-12 col-sm-8 p-3">
+                 <div class="col-9 col-sm-10 p-3">
           <h3 class="text-dark"><?php the_sub_field('field_5a86477e19328'); ?></h3>
          <?php the_sub_field('field_5a86437bed2a2'); ?>
         
--- loop-templates/acf/intro_page.php
+++ loop-templates/acf/intro_page.php
@@ -1,9 +1,3 @@
-<?php if (get_field('field_5a98cf8733d10')) : ?>
-<div id="instruction"><i class="fa fa-info-circle mr-1 fa-lg"></i>
-  <?php echo the_field('field_5a98cf8733d10'); ?>
-</div>
-<?php endif; ?>
-
 
 <style>
 .tab-content
