208f87d6056c9cf8cc3698c7de3ae98b01acfb1a
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-04-30 10:25:33 +1000

    Remove posts from theme

--- functions.php
+++ functions.php
@@ -224,10 +224,43 @@ if( function_exists('acf_add_options_page') ) {
 	
 	
 }
-//Dashboard content
+// Completely remove comments from the site
+
+// Removes from admin menu
+add_action( 'admin_menu', 'my_remove_admin_menus' );
+function my_remove_admin_menus() {
+    remove_menu_page( 'edit-comments.php' );
+}
+// Removes from post and pages
+add_action('init', 'remove_comment_support', 100);
+
+function remove_comment_support() {
+    remove_post_type_support( 'post', 'comments' );
+    remove_post_type_support( 'page', 'comments' );
+}
+// Removes from admin bar
+function mytheme_admin_bar_render() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('comments');
+}
+add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
+
+// Completely remove posts - we're only using pages
+
+function post_remove ()      //creating functions post_remove for removing menu item
+{ 
+   remove_menu_page('edit.php');
+}
+
+add_action('admin_menu', 'post_remove');   //adding action for triggering function call
 
 
-//Site support widget
+
+
+// Dashboard content
+
+
+// Site support widget
 
 function dashboard_widget_function( $post, $callback_args ) {
 	
