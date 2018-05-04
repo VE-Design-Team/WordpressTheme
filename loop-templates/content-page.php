73f1ede17281d3f67680beb0e7c82e5647344327
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-05-04 16:22:01 +1000

    Code tidy

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -1,22 +1,9 @@
-<?php
-/**
- * Partial template for content in page.php
- *
- * @package understrap
- */
-
-?>
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class();?> id="post-<?php the_ID();?>">
 
 	<div class="entry-content">
-
-		
-
 
 <?php get_template_part('loop-templates/acf');?>
 
 	</div><!-- .entry-content -->
-
-
 
 </article><!-- #post-## -->
--- page.php
+++ page.php
@@ -1,29 +1,25 @@
-<?php 
+<?php
 get_header();
 $sidebar_pos = get_theme_mod('understrap_sidebar_position');
 ?>
 
-
-	
-<?php edit_post_link('Update this content', '<div class="edit-me btn btn-sm small" title="Only logged in administrators see this">','</div>');?>
-	
+<?php edit_post_link('Update this content', '<div class="edit-me btn btn-sm small" title="Only logged in administrators see this">', '</div>');?>
 
 <div class="wrapper" id="page-wrapper">
 	<div id="content" tabindex="-1">
-			
+
 			<div class="pt-2 pb-2 content-area" id="primary">
 			<main class="site-main" id="main">
-			<?php if (get_field('field_5a98cf8733d10')) : ?>
+			<?php if (get_field('field_5a98cf8733d10')): ?>
 			<div class="instruction mr-1"><i class="fa fa-info-circle mr-1 fa-lg"></i>
   <?php echo the_field('field_5a98cf8733d10'); ?>
 </div>
-<?php endif; ?>
-				<?php while (have_posts()) : the_post(); ?>
-					<?php get_template_part('loop-templates/content', 'page'); ?>
-				<?php endwhile; // end of the loop.?>
-			</main><!-- #main -->
-		</div><!-- #primary -->
+<?php endif;?>
+<?php while (have_posts()): the_post();?>
+<?php get_template_part('loop-templates/content', 'page');?>
+<?php endwhile; // end of the loop.?>
+</main><!-- #main -->
+</div><!-- #primary -->
 </div><!-- Container end -->
 </div><!-- Wrapper end -->
-
-<?php get_footer(); ?>
+<?php get_footer();?>
