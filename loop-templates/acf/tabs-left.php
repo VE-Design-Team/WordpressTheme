a3eaf4ce95c9495325df59e7931eed5c7691c164
Jack Dunstan <jack.dunstan@rmit.edu.au>
2018-05-21 10:38:11 +1000

    triangles in left tabs

--- loop-templates/acf/tabs-left.php
+++ loop-templates/acf/tabs-left.php
@@ -2,16 +2,16 @@
  <div id="left-tabs">
 <div class="col-12">
     <div class="row">
-<div class="nav flex-column col-4" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+<div class="nav flex-column col-4 pr-0" id="v-pills-tab" role="tablist" aria-orientation="vertical">
   <!-- end setup tabs on left-->
   <?php while (have_rows('field_5a13c7fda9110')) : the_row();?>
   <a class=" nav-link text-right  " aria-hidden="true" id="v-pills-home-tab" data-toggle="pill" href="#<?php echo str_replace(str_split('\\()/:*?"<>,.| '), '', get_sub_field('field_5a14fdfcc7bba')); ?>" role="tab" aria-controls="v-pills-home" aria-selected="true"><h3 class="h5"><?php echo the_sub_field('field_5a14fdfcc7bba');?>
-</span></h3> </a><div class="triangle" ><div class="empty"></div></div> 
+</span><div class="triangle" ></div></h3> </a>
 <!-- triangle here -->
 
 <?php endwhile;?>
 </div>
-<div class="tab-content col-8 pt-1" id="v-pills-tabContent">
+<div class="tab-content col-8 pt-1 " id="v-pills-tabContent">
   <?php while (have_rows('field_5a13c7fda9110')) : the_row();?>
 
 <h3 class="h5 sr-only"><?php echo the_sub_field('field_5a14fdfcc7bba');?></h3>
