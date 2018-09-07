2a5b9d282f0e396be582592dbbdf7a7dc1947f72
Jack Dunstan <jack@dunsta.net>
2018-09-07 11:21:45 +1000

    SCORM builder features

--- functions.php
+++ functions.php
@@ -267,8 +267,8 @@ if (function_exists('acf_add_options_page')) {
 if (function_exists('acf_add_options_page')) {
 
     acf_add_options_page(array(
-        'page_title' => 'Analytics',
-        'menu_title' => 'Analytics',
+        'page_title' => 'Analytics and Settings',
+        'menu_title' => 'Custom Features',
         'menu_slug' => 'analytics-options',
         'capability' => 'edit_posts',
         'icon_url' => 'dashicons-chart-line', // Add this line and replace the second inverted commas with class of the icon you like
@@ -429,4 +429,62 @@ function custom_myme_types($mime_types){
   }
   add_filter('upload_mimes', 'custom_myme_types', 1, 1);
 
+//create post type SCORM
+  if ( ! function_exists('custom_post_type') ) {
 
+    // Register Custom Post Type
+    function custom_post_type() {
+    
+        $labels = array(
+            'name'                  => _x( 'scorm pages', 'Post Type General Name', 'text_domain' ),
+            'singular_name'         => _x( 'scorm', 'Post Type Singular Name', 'text_domain' ),
+            'menu_name'             => __( 'SCORM', 'text_domain' ),
+            'name_admin_bar'        => __( 'SCORM', 'text_domain' ),
+            'archives'              => __( 'Item Archives', 'text_domain' ),
+            'attributes'            => __( 'Item Attributes', 'text_domain' ),
+            'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+            'all_items'             => __( 'All Items', 'text_domain' ),
+            'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+            'add_new'               => __( 'Add New', 'text_domain' ),
+            'new_item'              => __( 'New Item', 'text_domain' ),
+            'edit_item'             => __( 'Edit Item', 'text_domain' ),
+            'update_item'           => __( 'Update Item', 'text_domain' ),
+            'view_item'             => __( 'View Item', 'text_domain' ),
+            'view_items'            => __( 'View Items', 'text_domain' ),
+            'search_items'          => __( 'Search Item', 'text_domain' ),
+            'not_found'             => __( 'Not found', 'text_domain' ),
+            'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+            'featured_image'        => __( 'Featured Image', 'text_domain' ),
+            'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+            'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+            'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+            'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+            'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+            'items_list'            => __( 'Items list', 'text_domain' ),
+            'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+            'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+        );
+        $args = array(
+            'label'                 => __( 'scorm', 'text_domain' ),
+            'description'           => __( 'SCORM page creator', 'text_domain' ),
+            'labels'                => $labels,
+            'supports'              => array( 'title', 'editor' ),
+            'hierarchical'          => true,
+            'public'                => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'menu_position'         => 5,
+            'show_in_admin_bar'     => true,
+            'show_in_nav_menus'     => true,
+            'can_export'            => true,
+            'has_archive'           => true,
+            'exclude_from_search'   => false,
+            'publicly_queryable'    => true,
+            'capability_type'       => 'page',
+        );
+        register_post_type( 'SCORM', $args );
+    
+    }
+    add_action( 'init', 'custom_post_type', 0 );
+    
+    }
