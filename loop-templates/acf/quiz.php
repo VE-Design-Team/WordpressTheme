3b394ca4aea17078e4e7efb11e30e13e29d55b31
Jack Dunstan <jack@dunsta.net>
2018-10-03 07:36:12 +1000

    WIP quiz

--- loop-templates/acf/quiz.php
+++ loop-templates/acf/quiz.php
@@ -1,3 +1,98 @@
+<div class="container-fluid" id="quiz">
+				  <div class="row rich-background">
+				    <div class="container">
+				      <div class="checkpage row glossarize">
+<!-- carousel for pages -->
+<div id="questions" class="carousel col-12" data-ride="carousel" data-interval="false">
+
+
+<!-- loop -->
+<?php
+
+// check if there are questions
+if( have_rows('field_5bb2038203e86') ):  ?>
+<?php $i = 0; ?>
+  <div class="carousel-inner row">
+ 	<?php // Get each of the questions
+    while ( have_rows('field_5bb2038203e86') ) : the_row(); ?>
+    
+<div class="col-12 carousel-item <?php if ($i == " 0 ") {echo "active ";}?>">
+     
+   
+      <?php  // Display the question
+        the_sub_field('field_5bb2039703e87');
+        // Display the question image
+        the_sub_field('field_5bb20bd5e1178'); ?>
+
+
+  
+
+
+ <?php if( have_rows('field_5bb20a29cf3f6') ):
+
+ 	// get each of the answers
+    while ( have_rows('field_5bb20a29cf3f6') ) : the_row();
+
+        // display an asnwer
+        the_sub_field('field_5bb20a3acf3f7');
+        // is this a correct asnwer?
+        the_sub_field('field_5bb20a41cf3f8');
+          // Feedback for this snaswer
+          the_sub_field('field_5bb20ad9cf3f9');
+
+
+
+    endwhile;
+
+else :
+
+    // No answers
+
+endif; ?> 
+
+
+        </div> 
+
+   <?php $i++; endwhile; ?>
+</div>
+<?php else :
+
+    // There are no questions
+
+endif;
+
+?>
+<!--loop -->
+
+  <!-- The slideshow -->
+
+   
+  
+
+  <!-- Left and right controls -->
+  <div class="controls row">
+  <a class="carousel-control-prev" href="#questions" data-slide="prev">
+    <i class="fa fa-chevron-circle-left fa-2x"></i>
+  </a>
+  <a class="carousel-control-next" href="#questions" data-slide="next">
+  <i class="fa fa-chevron-circle-right fa-2x"></i>
+</a>
+</div>
+
+  <!-- Indicators -->
+  <!--<ul class="carousel-indicators">
+    <li><a data-target="#demo" data-slide-to="0" class="active">1</a></li>
+    <li><a data-target="#demo" data-slide-to="1">2</a></li>
+    <li><a data-target="#demo" data-slide-to="2">3</a></li>
+  </ul>-->
+
+
+
+</div>
+
+                      </div></div></div></div>
+
+
 <?php
 
 // check if there are questions
