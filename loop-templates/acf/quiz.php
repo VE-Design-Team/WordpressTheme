745e493e5d966b641296172717282a71493330e9
Jack Dunstan <jack@dunsta.net>
2018-10-16 22:52:13 +1100

    Quiz finished

--- loop-templates/acf/quiz.php
+++ loop-templates/acf/quiz.php
@@ -1,18 +1,3 @@
-<style>
-
-  .hidden
-{
-  display: none;
-  position: absolute;
-}
-
-    </style>
-
-
-</head>
-
-
-
 <div class="container-fluid" id="quiz">
   <div class="row rich-background">
     <div class="container">
@@ -20,21 +5,20 @@
         <!-- carousel for pages -->
         <div id="questions" class="carousel col-12" data-ride="carousel" data-interval="false">
 
-
           <?php
 
 // check if there are questions
 if (have_rows('field_5bb2038203e86')):
-    $i = 1;
+    $q = 1;
     // Get each of the questions
     while (have_rows('field_5bb2038203e86')): the_row();?>
 
-          <div class="question<?php echo $i; ?> carousel-item <?php if ($i == " 1 ") {echo " active ";}?>">
+          <div class="question<?php echo $q; ?> carousel-item <?php if ($q == " 1 ") {echo " active ";}?>">
 
             <div class="row">
               <div class="col-12">
                 <h3 class="card-title">Question
-                  <?php echo $i; ?>
+                  <?php echo $q; ?>
                 </h3>
               </div>
               <div class="col-12 col-sm-6 col-md-4">
@@ -49,7 +33,6 @@ if (have_rows('field_5bb2038203e86')):
                   </p>
                   <form action="#">
 
-
                     <!-- get answers -->
                     <p>
                       <?php if (have_rows('field_5bb20a29cf3f6')):
@@ -57,14 +40,10 @@ if (have_rows('field_5bb2038203e86')):
 // get each of the answers
             while (have_rows('field_5bb20a29cf3f6')): the_row();?>
 
-
-
-
-                      <input class="with-gap" name="group<?php echo $i; ?>" type="radio" id="test<? echo $a; ?>" />
-                      <label for="test<? echo $a; ?>">
+                      <input class="with-gap" name="group<?php echo $q; ?>" type="radio" id="testq<?php echo $q; ?>a<? echo $a; ?>" />
+                      <label for="testq<? echo $a; ?>a<? echo $a; ?>">
                         <?php the_sub_field('field_5bb20a3acf3f7');?></label>
                       <br>
-
 
                       <?php $a++;endwhile;
 
@@ -75,60 +54,62 @@ if (have_rows('field_5bb2038203e86')):
         endif;?>
                       <!-- end get answers-->
 
-
-
                     </p>
-
 
                   </form>
                 </div>
                 <div class="answers">
                   <div class="card-action">
-                    <button id="question1" class="btn btn-primary btn-sm">Submit</span>
+                    <button id="question<?php echo $q; ?>" class="btn btn-primary btn-sm">Submit</span>
                   </div>
                 </div>
               </div>
 
-<!-- get results-->
+              <!-- get results-->
 
               <div class="results mt-5 col-12 ">
 
-<?php
-  // check if there are answers
+                <?php
+        // check if there are answers
         if (have_rows('field_5bb20a29cf3f6')):
-          $r = 1;
+            $r = 1;
             // get each of the answers
-            while (have_rows('field_5bb20a29cf3f6')): the_row(); ?>
-    <div id="q<?php echo $i; ?>a<?php echo $r;?>" class="hidden">
-                  <div class="card card-body">
+            while (have_rows('field_5bb20a29cf3f6')): the_row();?>
+                <div id="q<?php echo $q; ?>a<?php echo $r; ?>" class="hidden">
+                  <div class="card card-body correct<?php the_sub_field('field_5bb20a41cf3f8');?> ">
+                    <h4>
+                      <?php
+$t = get_sub_field('field_5bb20a41cf3f8');
 
-                <h4><?php 
-                // is this a correct asnwer?
-                the_sub_field('field_5bb20a41cf3f8'); ?></h4>
-                <?php // Feedback for this snaswer
-                the_sub_field('field_5bb20ad9cf3f9'); ?>
+if ($t == "1") {
+    echo "Correct";
+} else {
+    echo "Incorrect";
+}
+?>
+                    </h4>
 
-                    </div>
+                    <?php // Feedback for this snaswer
+                the_sub_field('field_5bb20ad9cf3f9');?>
 
-</div>
-                
-           <?php $r++; endwhile;
+                  </div>
+
+                </div>
+
+                <?php $r++;endwhile;
 
         else:
 
             // No answers
 
-        endif; ?>
+        endif;?>
 
-                 <!-- end get results-->
+                <!-- end get results-->
               </div>
             </div>
           </div>
 
-
-
-
-          <?php $i++;endwhile;
+          <?php $q++;endwhile;
 
 else:
 
@@ -166,22 +147,59 @@ endif;
             <div class="col"> <a class="carousel-control-next" href="#questions" data-slide="next"><i class="fa fa-chevron-circle-right fa-2x"></i></a></div>
           </div>
 
-
         </div>
       </div>
     </div>
   </div>
 </div>
 
-
 <script>
-  $('#question1').click(function () {
-    if ($('#test2').is(':checked')) {
-      $('#q1a1').toggleClass("hidden");
-    } else {
-      $('#q1a2').toggleClass("hidden");
+  <?php
+
+// check if there are questions
+if (have_rows('field_5bb2038203e86')):
+    $q = 1;
+    // Get each of the questions
+    while (have_rows('field_5bb2038203e86')): the_row();?>
+
+  $('#question<?php echo $q; ?>').click(function () {
+    if (1 > 5) { // do nothing of course
     }
+
+    <?php if (have_rows('field_5bb20a29cf3f6')):
+            $a = 1;
+// get each of the answers
+            while (have_rows('field_5bb20a29cf3f6')): the_row();?>
+
+    else if ($('#testq<?php echo $q; ?>a<?php echo $a; ?>').is(':checked')) {
+      $('#q<?php echo $q; ?>a<?php echo $a; ?>').removeClass("hidden");
+    }
+
+    <?php $a++;
+
+            endwhile;
+
+        else:
+
+// No answers
+
+        endif;?>
+
   });
 
+  <?php $q++;
+    endwhile;
 
+else:
+
+    // There are no questions
+
+endif;
+
+?>
+  //disable radios after click
+  $(":radio").click(function () {
+    var radioName = $(this).attr("name"); //Get radio name
+    $(":radio[name='" + radioName + "']").attr("disabled", true); //Disable all with the same name
+  });
 </script>
