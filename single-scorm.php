e72904396b7fbe2bc848eeb9161d8df5450146d7
Lawrence Makoona <lawrence.makoona@rmit.edu.au>
2018-11-27 16:45:56 +1100

    Designing a seperate template for the Splash page - To be cleaned up later on. Updating the stylesheet to reflect design - to be cleaned up.

--- single-scorm.php
+++ single-scorm.php
@@ -1,26 +1,58 @@
-<div class="wrapper">
-<nav id="sidebar">
-  <div id="" >
-      <!--   -->
-    <div><?php get_template_part( 'loop-templates/scorm/cde-scorm', 'nav' ); ?> </div>
-  </div>
-</nav>
+<?php get_template_part( 'loop-templates/scorm/fssi', 'header' ); ?>
 
-<div>
-<?php get_template_part( 'loop-templates/scorm/cde', 'header' ); ?>
-  <div class="container-fluid page-width">
+  <?php
+  if ( has_post_thumbnail($post->ID) ) { ?>
+
+  <?php  $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+  <div class="page-width splash-container">
+  <div class="splash">
+    <!--     content ----------------------------------------------------------------------->
+    <div id="content-nav" >
+
+      <div class="splash">
+        <div class="background" style="background-image: url('<?php echo $image[0]; ?>')">
+          <?php while ( have_posts() ) : the_post(); ?>
+              <div class="splash-banner">
+                <div class="splash-banner inner col-12">
+                  <div class="splash-left col-lg-3 col-md-3 col-sm-4 col-12 offset-1 offset-lg-2 offset-md-3 offset-sm-2" >
+                    <div class=" fssi-logo-text">
+
+                    </div>
+                  </div>
+                    <div class="splash-right col-lg-4 col-md-4 col-sm-4 col-12">
+                      <h2 class="fssi-splash-button">
+                        <?php the_title();?>
+                      </h2>
+                      <h3  class="fssi-splash-button">
+                        <?php if( get_field('field_5bdf9ea04ef39') ): ?>
+                        <a title="Next page" href="<?php the_field('field_5bdf9ea04ef39'); ?>">
+                        Start here</a>
+                        <?php endif; ?>
+                      </h3>
+                    </div>
+                  </div>
+              </div>  <?php endwhile; // end of the loop. ?>
+            </div>
+
+        </div>
+      </div>
+
+  </div>
+</div>
+   <?php ?>
+  <?php }
+  else { ?>
+    <div class="container-fluid page-width">
     <div class="row">
+      <div id="accordion-move" >
+        <?php get_template_part( 'loop-templates/scorm/scorm', 'nav' ); ?>
+      </div>
       <!--     content ----------------------------------------------------------------------->
       <div id="content" >
-        <div id="push" dir=""class="row">
-          <div>
-              <button type="button" id="sidebarCollapse" class="">
-                  <span class="fas"></span>
-              </button>
-          </div>
-          <div class="container-fluid">
+        <div class="row">
+          <div class="container">
             <?php while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'loop-templates/scorm/cde-scorm', 'content' ); ?>
+            <?php get_template_part( 'loop-templates/scorm/scorm', 'content' ); ?>
             <?php endwhile; // end of the loop. ?>
             <!--   col-10 -->
           </div>
@@ -28,11 +60,11 @@
       </div>
     </div>
   </div>
+  <?php get_template_part( 'loop-templates/scorm/fssi', 'footer' ); ?>
+  <?php }
+  ?>
 
-</div>
-</div>
-<div class="b-0 container-fluid page-width m-0 cde_footer">
-  <?php get_template_part( 'loop-templates/scorm/cde', 'bottom_navigation' ); ?>
-</div>
-<!-- content ----------------------------------->
-<?php get_template_part( 'loop-templates/scorm/cde', 'footer' ); ?>
+
+
+
+<!------------------------------------------ content ----------------------------------->
