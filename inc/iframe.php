e11c454856b94a90112d03aea78d52c2a899c957
Jack Dunstan <jack@dunsta.net>
2018-12-18 16:11:30 +1100

    Force reload to top of SCORM page when SCORM page navigates to new page

--- inc/iframe.php
+++ inc/iframe.php
@@ -10,7 +10,7 @@ function iframe_admin_notice()
                       <h2>Embed this page in Canvas by pasting this code <span onclick="copyClipboard()" title="Click here to copy to clipboard" class="dashicons dashicons-paperclip" style="cursor: pointer; color: #0073aa;"></span></h2>  <div id="divClipboard">
 
 
-&lt;iframe src="' . get_home_url() . '/?p=' . get_the_ID() . '" title="' . $iframe_title . '" style="border:0px #ffffff none;" id="C3319iframe"   width="1200" height="400" name="Richcontent" scrolling="no" frameborder="0" onload="resizeIframe(this)" allowfullscreen="allowfullscreen" onload="iFrameResize()" &gt;&lt;/iframe&gt;
+&lt;iframe src="' . get_home_url() . '/?p=' . get_the_ID() . '" title="' . $iframe_title . '" style="border:0px #ffffff none;" id="C3319iframe"   width="1200" height="400" name="Richcontent" scrolling="no" frameborder="0" onload="resizeIframe(this); scroll(0,0);" allowfullscreen="allowfullscreen" onload="iFrameResize()" &gt;&lt;/iframe&gt;
                   </div>      </div>
          ';
     }
