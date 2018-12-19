09cf81bba400651bf4134ec18c352eec6c7afdc0
Lawrence Makoona <lawrence.makoona@rmit.edu.au>
2018-12-20 10:09:28 +1100

    Adding CDE template files

--- loop-templates/scorm/cde-scorm-content.php
+++ loop-templates/scorm/cde-scorm-content.php
@@ -1,12 +1,10 @@
 <h1><?php the_title(); ?></h1>
 <div class="heading-underline" style=""></div>
+<article><?php the_content(); ?></article>
 <?php if (get_field('field_5ade80680cd32')): ?>
 				<div class="instruction mr-1"><i class="fa fa-info-circle mr-1 fa-lg"></i>
 	  <?php echo the_field('field_5ade80680cd32'); ?>
 	</div>
 <?php endif;?>
-<article><?php the_content(); ?></article>
 <section><?php get_template_part( 'loop-templates/acf' ); ?></section>
 <article><?php the_field('field_5b91d32f52a61'); ?></article>
-
-test2
