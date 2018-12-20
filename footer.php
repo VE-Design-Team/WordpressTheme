a4cf3ffe040c7dda25e448a8ece4817a9126bcb0
Jack Dunstan <jack@dunsta.net>
2018-12-20 11:41:43 +1100

    Removed redundant code

--- footer.php
+++ footer.php
@@ -9,38 +9,5 @@
 $the_theme = wp_get_theme();
 ?>
 <?php wp_footer(); ?>
-<?php if (get_field('field_5a13c5a7ec802') == 'Tooltip Textstrings'): {}; ?>
-
-<?php elseif (get_field('field_5a13c5a7ec802') == 'Hotspots'): {}?>  
-
-  <!--TODO fix this tooltip conflict -->
-<?php else: {} ?>
-  <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/tooltip.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.glossarize.js"></script>
-<?php endif;?>
-
-  <script>
-  $(function(){
-    $('.glossarize').glossarizer({
-      //lookupTagName: '',
-      lookupTagName: 'p, li',
-      replaceTag: 'abbr',
-      
-      <?php if (WP_DEBUG === true) {
-    echo "//dev environment
-    ";
-    echo "sourceURL: '".get_site_url()."/glossarydata/',";
-} else {
-    echo "//live environment
-   ";
-    echo "sourceURL: '".get_site_url()."/index.php/glossarydata/',";
-} ?>
-      //sourceURL: '<?php echo get_stylesheet_directory_uri(); ?>/glossary.json',
-      callback: function(){
-        new tooltip();
-      }
-    });
-  });
-  </script>
 </body>
 </html>
