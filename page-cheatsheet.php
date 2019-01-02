8e58e74a055523d9ca68a0c3a702a06da1514718
Jack Dunstan <jack.dunstan@rmit.edu.au>
2019-01-02 15:03:19 +1100

    Improvement to code

--- page-cheatsheet.php
+++ page-cheatsheet.php
@@ -9,40 +9,98 @@ pre{
 	padding: 1rem;
 		border-radius: 0.5rem;
 }
+body {
+  background: #eee;
+  padding: 20px;
+}
 
+article {
+  max-width: 50em;
+  background: white;
+  padding: 2em;
+  margin: 1em auto;
+}
+
+.table-of-contents {
+ 
+  
+  background: #eee;
+  font-size: 0.8em;
+  padding: 1em 2em;
+  margin: 0 0 0.5em 0.5em;
+}
+.table-of-contents ul {
+  padding: 1rem;
+}
+.table-of-contents li {
+  margin: 0 0 0.25em 0;
+}
+.table-of-contents a {
+  text-decoration: none;
+}
+.table-of-contents a:hover,
+.table-of-contents a:active {
+  text-decoration: underline;
+}
+
+h3:target {
+  animation: highlight 1s ease;
+}
+
+@keyframes highlight {
+  from { background: yellow; }
+  to { background: white; }
+}
 </style>
 
 <div class="container">
 	<div id="content" tabindex="-1"	>
 			<h1>Cheatsheet</h1>
 
-			<div class="pt-2 pb-2 content-area" id="primary">
-			<main class="site-main" id="main">
-			<?php if (get_field('field_5a98cf8733d10')): ?>
-
-<?php endif;?>
-<?php while (have_posts()): the_post();?>
+	<article>
+ 
+   <div class="cheats">
+  
+  <?php while (have_posts()): the_post();?>
 <?php  get_template_part('loop-templates/content', 'page');?>
 <?php endwhile; // end of the loop.?>
+  </div>
+  
+</article>
+
 </main><!-- #main -->
 </div><!-- #primary -->
 </div><!-- Container end -->
 </div><!-- Wrapper end -->
 <?php get_footer();?>
 <script>
-(function() {
-  const headings = document.querySelectorAll('h2');
-  
-  Array.prototype.forEach.call(headings, h => {
-    let btn = h.querySelector('button');
-    let target = h.nextElementSibling;
-    
-    btn.onclick = () => {
-      let expanded = btn.getAttribute('aria-expanded') === 'true';
-      
-      btn.setAttribute('aria-expanded', !expanded);
-      target.hidden = expanded;  
-    }
-  });
-})()
+var ToC =
+  "<nav role='navigation' class='table-of-contents'>" +
+    "<h3>On this page:</h3>" +
+    "<ul>";
+
+var newLine, el, title, link;
+
+$("article h2").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+
+  ToC += newLine;
+
+});
+
+ToC +=
+   "</ul>" +
+  "</nav>";
+
+$(".cheats").prepend(ToC);
 </script>
