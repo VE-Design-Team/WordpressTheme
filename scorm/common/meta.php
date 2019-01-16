1df1b658a72bbaeba6cabb3bb3662c2a92ea8216
Jack Dunstan <jack.dunstan@rmit.edu.au>
2019-01-16 15:11:00 +1100

    Added copyright message to site meta

--- scorm/common/meta.php
+++ scorm/common/meta.php
@@ -1,3 +1,3 @@
 <meta name='revised' content='<?php the_modified_date(); ?>'>
 <meta name='designer' content='RMIT - VE Design Team'>
-<meta name='copyright' content='All rights reserved - Future Social Services Institute'>
+<meta name='copyright' content='<?php echo get_field('field_5c3d3a8832610', 'option'); ?><?php echo date("Y");?>'>
