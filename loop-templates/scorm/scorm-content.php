54a055328cc1683e74b6fc776e35640c999d4702
Jack Dunstan <jack.dunstan@rmit.edu.au>
2019-01-22 16:59:21 +1100

    Intro page improvements

--- loop-templates/scorm/scorm-content.php
+++ loop-templates/scorm/scorm-content.php
@@ -22,3 +22,17 @@ else { ?>
 <article class="mt-3">
     <?php the_field('field_5b91d32f52a61');?>
 </article>
+
+<?php if (get_field('field_5a13c5a7ec802') == 'Introduction page'): ?>
+<div class="legend" style="background-color: #eee;">
+
+<h3 style="font-family: Trade Gothic LT Pro Cn !important; font-weight: bold; line-height: 1.2; color: #3b3d3e; font-size: 1.5rem;">Start here</h3>
+<img class="alignnone wp-image-1907" src="<?php echo get_stylesheet_directory_uri()?>/fssi/img/theme/svg/next.svg" alt="" width="33" height="30" />   Work through this module using the arrows at the bottom of the page.
+<br>
+<br>
+<img class="alignnone wp-image-1906" src="<?php echo get_stylesheet_directory_uri()?>/fssi/img/theme/svg/hamburger.svg" alt="" width="33" height="28" />   The navigation menu will help you keep track of the course.
+<br>
+<br>
+<img class="alignnone wp-image-1908" src="<?php echo get_stylesheet_directory_uri()?>/fssi/img/theme/svg/fullscreen.svg" alt="" width="32" height="34" />   Toggle the full screen button for a distraction free experience.
+</div>
+<?php endif;?>
