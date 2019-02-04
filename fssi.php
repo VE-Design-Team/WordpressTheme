8ecad493019711b0506826add4cd0844c202fc6d
Jack Dunstan <jack.dunstan@rmit.edu.au>
2019-02-04 18:17:34 +1100

    Auto stash before merge of "master" and "page-builder/master"

--- fssi.php
+++ fssi.php
@@ -69,7 +69,10 @@
       <!-- footer buttons-->
       <?php if ( has_post_thumbnail($post->ID) && get_field('field_5be47786ed504') == 'home-page'): ?>
       <!--
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
 <div style="clear:both;"></div>
 <div id="footer" class="col-12 p-0 m-0">
