68175ba2c0fc2a4c1acfc0b4c026827cb2c32b38
James <james@firstloveprojects.com>
2019-02-21 13:13:02 +1100

    Update accordian.php
    Enabled self collapses behaviour for accordian.

--- loop-templates/acf/accordion.php
+++ loop-templates/acf/accordion.php
@@ -42,18 +42,18 @@ $('.card-header').click(function(){
 
 <?php else: ?>
 <?php if (have_rows('field_5a13c61e91a66')): ?>
-<div id="accordion" class="mr-1 mb-3">
 <div id="cards" class="expand">
+<div id="accordion" class="mr-1 mb-3">
  <?php while (have_rows('field_5a13c61e91a66')): the_row();?>
- <div class="card expand">
-<h3 class="card-header collapsed h6" tabindex="0" id="heading<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>"
- data-toggle="collapse"  data-target="#collapse<?php echo str_replace(str_split('\\/:*?" <>(),.| '), '', get_sub_field('field_5a13c67d91a67')); ?>" aria-expanded="true" aria-controls="collapse<?php echo str_replace(str_split('\\/:*?"<>,.()| '), '', get_sub_field('field_5a13c67d91a67')); ?>">
+ <div id="cards" class="expand card">
+<h3 class="card-header collapsed h6" role="button" tabindex="0" id="heading<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>"
+ data-toggle="collapse"  data-target="#collapse<?php echo str_replace(str_split('\\/:*?" <>(),.| '), '', get_sub_field('field_5a13c67d91a67')); ?>" aria-expanded="false" 
+ aria-controls="collapse<?php echo str_replace(str_split('\\/:*?"<>,.()| '), '', get_sub_field('field_5a13c67d91a67')); ?>">
 						<i class="fa fa-plus mr-1"></i>
 						<i class="fa fa-minus mr-1 mr-1"></i>
 						<?php echo get_sub_field('field_5a13c67d91a67'); ?>
 					</h3>
-					<span id="collapse<?php echo str_replace(str_split('\\/:*?" <>,.()| '), '', get_sub_field('field_5a13c67d91a67')); ?>" class="collapse" aria-labelledby="heading
-						<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>" >
+					<span id="collapse<?php echo str_replace(str_split('\\/:*?"<>,.()| '), '', get_sub_field('field_5a13c67d91a67')); ?>" class="collapse" aria-labelledby="heading<?php echo str_replace(' ', '', get_sub_field('field_5a13c67d91a67')); ?>" data-parent="#accordion">
 						<div class="card-body glossarize">
 							<?php echo the_sub_field('field_5a13c6ac91a68'); ?>
 						</div>
