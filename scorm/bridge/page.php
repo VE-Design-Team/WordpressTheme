02f6cb92af6dba69ddd1d93daab7b1d74ed969d6
Jack Dunstan <jack@dunsta.net>
2019-05-21 06:28:46 +1000

    Prev-next navigation on Bridge SCORM

--- scorm/bridge/page.php
+++ scorm/bridge/page.php
@@ -3,7 +3,7 @@
     <div class="row">
       <!-- bridge content ----------------------------------------------------------------------->
       <div id="content" >
-        <div id="push" dir=""class="row">
+        <div>
 
           <div class="container">
             <?php while ( have_posts() ) : the_post(); ?>
@@ -14,4 +14,18 @@
         </div>
       </div>
     </div>
+    <div class="bridge-nav">
+
+<?php if( get_field('field_5bdf9ede4ef3a') ): ?>
+             <a title="Previous page" href="<?php the_field('field_5bdf9ede4ef3a'); ?>"  class="btn btn-sm btn-prev">
+             Prev </a>
+             <?php endif; ?>
+
+             <?php if( get_field('field_5bdf9ea04ef39') ): ?>
+             <a title="Next page" href="<?php the_field('field_5bdf9ea04ef39'); ?>"  class="float-right btn btn-sm btn-next">
+             Next
+             </a>
+             <?php endif; ?>
+
+</div>
   </div>
