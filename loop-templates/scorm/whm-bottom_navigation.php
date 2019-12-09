fe19ef05238672bffc3c6c920c238d9ce7cc5a4a
Lawrence <lawrence.makoona@rmit.edu.au>
2019-12-09 13:14:12 +1100

    Moving Next Prev button so that Next comes first in Mobile

--- loop-templates/scorm/whm-bottom_navigation.php
+++ loop-templates/scorm/whm-bottom_navigation.php
@@ -2,6 +2,17 @@
 <div class="" style="padding-bottom: 50px;">
           <div id="bottom_navigation" class="">
             <div id="next-prev">
+              <?php
+
+              // vars
+              $next = get_field('field_5bdf9ea04ef39', false, false);
+
+              // check
+              if( $next ): ?>
+              <div class="next-page">
+              <a class="float-right material-card" title="Next page" href="<?php echo get_the_permalink($next); ?>">Next page: <strong><?php echo get_the_title($next); ?></strong></a>
+              </div>
+              <?php endif; ?>
 
 <?php
 
@@ -12,17 +23,6 @@ $prev = get_field('field_5bdf9ede4ef3a', false, false);
 if( $prev ): ?>
 <div class="prev-page">
 <a class="material-card float-left " title="Previous page" href="<?php echo get_the_permalink($prev); ?>">Previous page: <strong><?php echo get_the_title($prev); ?></strong></a>
-</div>
-<?php endif; ?>
-<?php
-
-// vars
-$next = get_field('field_5bdf9ea04ef39', false, false);
-
-// check
-if( $next ): ?>
-<div class="next-page">
-<a class="float-right material-card" title="Next page" href="<?php echo get_the_permalink($next); ?>">Next page: <strong><?php echo get_the_title($next); ?></strong></a>
 </div>
 <?php endif; ?>
 
