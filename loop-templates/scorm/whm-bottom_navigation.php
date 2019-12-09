d171701ecbafb6c357b15890758385138c7be503
Lawrence <lawrence.makoona@rmit.edu.au>
2019-12-09 13:19:26 +1100

    Fixing bottom navigation

--- loop-templates/scorm/whm-bottom_navigation.php
+++ loop-templates/scorm/whm-bottom_navigation.php
@@ -23,19 +23,8 @@ if( $prev ): ?>
 <a class="float-left" title="Previous page" href="<?php echo get_the_permalink($prev); ?>">Previous page: <strong><?php echo get_the_title($prev); ?></strong></a>
 
 <?php endif; ?>
-<<<<<<< HEAD
-=======
-<?php
 
-// vars
-$next = get_field('field_5bdf9ea04ef39', false, false);
 
-// check
-if( $next ): ?>
-<a class="float-right" title="Next page" href="<?php echo get_the_permalink($next); ?>">Next page: <strong><?php echo get_the_title($next); ?></strong></a>
-
-<?php endif; ?>
->>>>>>> c8d635242f5379c41004ee71bce2cb2fc61eb755
 
             </div>
           </div>
