a6fd8e8a955b17155208114ba01656f5c5f63311
Lawrence <lawrence.makoona@rmit.edu.au>
2019-12-16 15:32:56 +1100

    Updaing Quick link card - Adding Description and tidying up URLs

--- loop-templates/acf/socialmedia-feed.php
+++ loop-templates/acf/socialmedia-feed.php
@@ -1,6 +1,6 @@
-<script src="/wordpress481/wp-content/themes/page-builder/js/aos.js"></script>
-<script src="/wordpress481/wp-content/themes/page-builder/js/animate.js"></script>
-<link href="/wordpress481/wp-content/themes/page-builder/css/aos.css" rel="stylesheet">
+<script src="<?php echo get_stylesheet_directory_uri();?>/js/aos.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri();?>/js/animate.js"></script>
+<link href="<?php echo get_stylesheet_directory_uri();?>/css/aos.css" rel="stylesheet">
 <div class="socialmediafeed">
 <!--Staff Section starts - e.g staff reporting directly to the CEO -->
 <?php
@@ -42,8 +42,8 @@ if (get_sub_field('field_5db91fdfc957c')) {
 }
 ?>
 
-              <div class="social-green social-right pt-1 pb-2" > <img src="/wordpress481/wp-content/uploads/2019/11/like.svg" alt="like" height="23" width="23"></div>
-              <div class="social-green social-right pt-1 pb-2" > <img src="/wordpress481/wp-content/uploads/2019/11/share.svg" alt="share" height="23" width="23"></div>
+              <div class="social-green social-right pt-1 pb-2" > <img src="<?php echo get_stylesheet_directory_uri();?>/img/like.svg" alt="like" height="23" width="23"></div>
+              <div class="social-green social-right pt-1 pb-2" > <img src="<?php echo get_stylesheet_directory_uri();?>/img/share.svg" alt="share" height="23" width="23"></div>
 </div>
 </div>
 
@@ -62,14 +62,12 @@ $replies = get_sub_field('field_5db9211db6c12');
 if (is_array($replies)) {
   $count = count($replies) ;
 }
-
 if ($count > 1 ) { echo "<div class='x-replies'>"."$count replies"."</div>";
 }
 elseif  ($count < 1 ) { echo "";
 }
 elseif  ($count < 2 ) { echo "<div class='x-replies'>"."$count reply"."</div>";
 }
-
 ?>
     <?php
     // check if the repeater field has rows of data for Staffs
@@ -112,11 +110,9 @@ elseif  ($count < 2 ) { echo "<div class='x-replies'>"."$count reply"."</div>";
                             .cls-1 {
                               clip-path: url(#clip-like);
                             }
-
                             .cls-2 {
                               fill: rgba(255,255,255,0);
                             }
-
                             .cls-3 {
                               fill: #888;
                             }
@@ -130,7 +126,7 @@ elseif  ($count < 2 ) { echo "<div class='x-replies'>"."$count reply"."</div>";
                           <path id="noun_like_163915-2" data-name="noun_like_163915" class="cls-3" d="M28.841,21.208a2.233,2.233,0,0,0-.48-1.253,1.928,1.928,0,0,0-.449-2.236c-1.3-1.186-4.365-.542-4.71-.465h0c-.2.035-.4.076-.613.124a5.938,5.938,0,0,1,.455-2.178c.51-1.548.48-2.733-.089-3.52a2.291,2.291,0,0,0-1.835-.891.87.87,0,0,0-.679.311,2.507,2.507,0,0,0-.3,1.729c-.48,1.293-1.841,4.489-3,5.377a.451.451,0,0,0-.053.048,3.846,3.846,0,0,0-.725,1.1,1.48,1.48,0,0,0-.741-.2H13.384a1.492,1.492,0,0,0-1.49,1.49V26.6a1.492,1.492,0,0,0,1.49,1.49h2.234a1.481,1.481,0,0,0,.9-.3l.83.127h.01a36.812,36.812,0,0,0,4.879.268q.654.051,1.224.051a8.282,8.282,0,0,0,1.7-.151,3.16,3.16,0,0,0,2.253-1.4,2.208,2.208,0,0,0,.242-1.4,2.317,2.317,0,0,0,.68-2.631A2.161,2.161,0,0,0,28.841,21.208ZM13.384,27.166a.569.569,0,0,1-.569-.569V20.649a.569.569,0,0,1,.569-.569h2.234a.569.569,0,0,1,.472.252.46.46,0,0,0,.088.219.563.563,0,0,1,.009.1V26.6a.569.569,0,0,1-.569.569Zm14.067-4.909a.456.456,0,0,0-.063.554,1.43,1.43,0,0,1-.526,1.925.461.461,0,0,0-.157.524,1.3,1.3,0,0,1-.1.972,2.312,2.312,0,0,1-1.639.944,9.742,9.742,0,0,1-2.679.081H22.24A36.554,36.554,0,0,1,17.482,27l-.413-.063a1.478,1.478,0,0,0,.04-.337V20.649a1.482,1.482,0,0,0-.071-.453,3.1,3.1,0,0,1,.691-1.281c1.636-1.295,3.244-5.688,3.312-5.875a.463.463,0,0,0,.022-.228,2.168,2.168,0,0,1,.063-1.1,1.358,1.358,0,0,1,1.077.509,3.141,3.141,0,0,1-.04,2.691c-.609,1.848-.666,2.817-.19,3.24a.764.764,0,0,0,.766.137c.223-.051.439-.1.642-.131l.044-.01c1.13-.248,3.157-.4,3.865.25a1.084,1.084,0,0,1,.131,1.377.461.461,0,0,0,.081.593,1.226,1.226,0,0,1-.051,1.888ZM13.66,25.912a.723.723,0,1,1,.723.723A.723.723,0,0,1,13.66,25.912Z" transform="translate(-8.871 -7.791)"/>
                         </g>
                       </svg></div>
-                      <div class="social-green social-right pt-1 pb-2" > <img  src="/wordpress481/wp-content/uploads/2019/11/share.svg" alt="share" height="23" width="23"></div>
+                      <div class="social-green social-right pt-1 pb-2" > <img  src="<?php echo get_stylesheet_directory_uri();?>/img/share.svg" alt="share" height="23" width="23"></div>
         </div>
 
 
